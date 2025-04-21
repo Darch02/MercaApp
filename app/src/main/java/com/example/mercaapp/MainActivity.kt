386bdd36490c6130528aefee76dbe2bdc338b89d
Darch02 <juanajaramillom@hotmail.com>
2025-04-20 19:21:22 -0500

    se volvió a crear el proyecto, con su primera pantalla

---
+++ app/src/main/java/com/example/mercaapp/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.example.mercaapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.mercaapp.ui.theme.MercaAppTheme
+import androidx. compose. ui. text. style. TextAlign
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MercaAppTheme(dynamicColor = false) {
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    OnboardingScreen()
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier = Modifier
+) {
+
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bienvenido a",
+            style = MaterialTheme.typography.titleLarge
+        )
+        Text(
+            text = "MercaApp",
+            style = MaterialTheme.typography.titleLarge,
+            color = MaterialTheme.colorScheme.primary
+        )
+        Text(
+            text = "Aquí podrás crear tus listas de mercado, tener un inventario y mucho más!",
+            style = MaterialTheme.typography.bodyLarge.copy(
+                textAlign = TextAlign.Center
+
+            ),
+            color = MaterialTheme.colorScheme.primary,
+            modifier = Modifier.padding(vertical = 24.dp).width(331.dp)
+        )
+        Button(modifier = Modifier.padding(vertical = 24.dp).width(225.dp), onClick = {}) {
+            Text("Continuar")
+        }
+        Row () {
+            Text(
+                text = "¿Ya tienes una cuenta? ",
+                style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+                text = "Inicia sesión",
+                style = MaterialTheme.typography.bodyLarge.copy(
+                    fontWeight = FontWeight.Bold
+                )
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyAppPreview() {
+    MercaAppTheme(dynamicColor = false) {
+       OnboardingScreen()
+    }
+}
---
+++ app/src/main/java/com/example/mercaapp/ui/theme/Type.kt
@@ -0,0 +1,40 @@
+package com.example.mercaapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.example.mercaapp.R
+
+// Set of Material typography styles to start with
+val MyFont = FontFamily(
+    Font(R.font.instrumentsans_regular, FontWeight.Normal),
+    Font(R.font.instrumentsans_bold, FontWeight.Bold)
+)
+
+
+val Typography = Typography(
+    bodyLarge = TextStyle(
+        fontFamily = MyFont,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = MyFont,
+        fontWeight = FontWeight.Bold,
+        fontSize = 48.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+)
